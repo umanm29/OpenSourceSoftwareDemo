04ad9e6695757ed3618669020107073b5b8e2beb
umanm29 <112383317+umanm29@users.noreply.github.com>
2022-09-10 22:46:41 +0530

    Updated

--- First.java
+++ First.java
@@ -1,5 +1,6 @@
 public class JavaExample {
   public static void main(String[] args) {
     System.out.println("OSS Lab session);
+    System.out.println("Open Source Software Lab session-1 on Saturday 10th Sept 22);
   }
 }
