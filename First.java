78e6c343ffe692baeb51d670cbbc0e4e7c5ebf17
umanm29 <112383317+umanm29@users.noreply.github.com>
2022-09-10 22:31:26 +0530

    Added a Print Statement

--- First.java
+++ First.java
@@ -1,5 +1,6 @@
 public class JavaExample {
   public static void main(String[] args) {
     System.out.println("OSS Lab session);
+    System.out.println("Open source Software lab 1st session on Saturday 10th");
   }
 }
